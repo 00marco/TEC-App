00335cd4d3f557d3b175b7d500adf19b17a44dc1
MarcoPulido <MarcoPulido@DESKTOP-8K7EJ6S>
2019-10-15 22:46:55 +0800

    worked on remove candidate.

--- TEC-App/Views/IndividualCandidateDetailsView/IndividualCandidateDetailsView_ViewModel.cs
+++ TEC-App/Views/IndividualCandidateDetailsView/IndividualCandidateDetailsView_ViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
 using GalaSoft.MvvmLight.CommandWpf;
 using GalaSoft.MvvmLight.Messaging;
 using TEC_App.Messages;
 using TEC_App.Models.Db;
+using TEC_App.Services.AddressCandidateService;
+using TEC_App.Services.CandidateQualificationService;
+using TEC_App.Services.CandidateSessionService;
 using TEC_App.Services.EmployeeService;
+using TEC_App.Services.JobHistoryService;
 using TEC_App.ViewModels;
 using TEC_App.Views.CandidateView;
 
@@ -16,11 +21,15 @@ namespace TEC_App.Views.IndividualCandidateDetailsView
         private Candidate_Session _selectedCandidateSession;
         private Address_Candidate _selectedAddressCandidate;
 
-        public IndividualCandidateDetailsView_ViewModel(ICandidateService candidateService)
+        public IndividualCandidateDetailsView_ViewModel(ICandidateService candidateService, IAddressCandidateService addressCandidateService, ICandidateSessionService candidateSessionService, ICandidateQualificationService candidateQualificationService, IJobHistoryService jobHistoryService)
         {
             CandidateService = candidateService;
 			Messenger.Default.Register<ViewCandidateDetailsMessage>(this, NotifyMe);
-
+            CandidateService = candidateService;
+            AddressCandidateService = addressCandidateService;
+            CandidateSessionService = candidateSessionService;
+            CandidateQualificationService = candidateQualificationService;
+            JobHistoryService = jobHistoryService;
         }
 
         private void NotifyMe(ViewCandidateDetailsMessage obj)
@@ -35,6 +44,11 @@ namespace TEC_App.Views.IndividualCandidateDetailsView
 
         public Candidate Candidate { get; set; }
         public ICandidateService CandidateService { get; set; }
+        public IAddressCandidateService AddressCandidateService { get; set; }
+        public ICandidateSessionService CandidateSessionService { get; set; }
+        public ICandidateQualificationService CandidateQualificationService { get; set; }
+        public IJobHistoryService JobHistoryService { get; set; }
+
 
         public Address_Candidate SelectedAddressCandidate
         {
@@ -66,6 +80,25 @@ namespace TEC_App.Views.IndividualCandidateDetailsView
 		private void BackProc()
 		{
 			Messenger.Default.Send(new NotificationMessage(nameof(CandidateView_ViewModel)));
-		}
-	}
+            Messenger.Default.Send(new LoadCandidateViewMessage());
+        }
+
+        public ICommand RemoveCommand => new RelayCommand(Remove);
+
+        private void Remove()
+        {
+            if (MessageBox.Show("Are you sure?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                CandidateService.RemoveCandidate(Candidate);
+                BackProc();
+            }
+        }
+
+        public ICommand UpdateCommand => new RelayCommand(UpdateProc);
+
+        private void UpdateProc()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
